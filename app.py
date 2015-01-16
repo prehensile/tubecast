90160b2905ee296188e1448e5ebe783d61f34f8d
prehensile <me@prehensile.co.uk>
2015-01-16 17:55:01 +0000

    new feed template & parsing, improved file format handling

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ import datetime, time
 from email import utils
 from werkzeug.wrappers import Response as ResponseBase
 from itunes_categories import valid_categories
+from feedgen.feed import FeedGenerator
+
+MP3_BITRATE_K = 128
+# AUDIO_FORMAT = "mp3"
+# MIME_TYPE = "audio/mpeg"
+AUDIO_FORMAT_MP3 = "mp3"
+AUDIO_FORMAT_ADTS = "adts"
+
+AUDIO_FORMAT = AUDIO_FORMAT_MP3
+MIME_TYPE = "audio/mp3"
 
 class StreamingResponse( ResponseBase ):
     
-    def __init__( self, youtube_id ):
+    def __init__( self, youtube_id=None, audio_format=None, headers=None ):
         
         # construct path to heroku vended ffmpeg first...
         ff_path = os.path.dirname(os.path.realpath(__file__))
@@ -23,18 +33,20 @@ class StreamingResponse( ResponseBase ):
             ff_path = "ffmpeg"
             is_heroku = False
 
-        acodec = "libmp3lame"
-        #acodec = "libvo_aacenc"
-        # if is_heroku:
-        #     acodec = "libfaac"
         yt_args = [ "youtube-dl", "-f", "140", "-q", "--output", "-", youtube_id ]
-        ff_args = [ ff_path, "-loglevel", "quiet", "-i", "-", "-acodec", acodec, "-ab", "128k", "-ac", "2", "-ar", "44100", "-f", "mp3", "-" ]
-        #ff_args = [ ff_path, "-loglevel", "quiet", "-i", "-", "-vn", "-acodec", acodec, "-f", "adts", "-" ]
+        if audio_format == AUDIO_FORMAT_MP3:
+            br = "%sk" % MP3_BITRATE_K
+            acodec = "libmp3lame"
+            ff_args = [ ff_path, "-loglevel", "quiet", "-i", "-", "-acodec", acodec, "-ab", br, "-ac", "2", "-ar", "44100", "-f", audio_format, "-" ]
+        else:
+            acodec = "libvo_aacenc"
+            if is_heroku:
+                # version of ffmpeg in heroku buildpack doesn't contain libvo_aacenc codec
+                acodec = "libfaac"
+            ff_args = [ ff_path, "-loglevel", "quiet", "-i", "-", "-vn", "-acodec", acodec, "-f", "adts", "-" ]
+
         #ff_args = [ ff_path, "-i", "-", "-acodec", "copy", "-vn", "-f", "adts", "-" ]
         #ff_args = [ ff_path, "-i", "-", "-acodec", "copy", "-vn", "-f", "mp4", "-movflags", "frag_keyframe", "-frag_size", "1024", "-" ]
-        #filename = "%s.aac" % youtube_id
-        #filename = "%s.adts" % youtube_id
-        filename = "%s.mp3" % youtube_id
 
         print " ".join(yt_args)
         print " ".join(ff_args)
@@ -67,15 +79,15 @@ class StreamingResponse( ResponseBase ):
         status = 500
         mimetype = None
         response = "Error"
-        
+
         if (self._yt_proc is not None) and (self._ff_proc is not None):
             response = stream()
             status = 200
-            mimetype = app.config['MIME_TYPE']
+            mimetype = MIME_TYPE
 
         super( StreamingResponse, self ).__init__( response=response,
                                                     status=status,
-                                                    headers={'Content-Disposition' : 'attachment; filename=%s' % filename},
+                                                    headers=headers,
                                                     mimetype=mimetype,
                                                     content_type=None,
                                                     direct_passthrough=False ) 
@@ -109,7 +121,6 @@ class StreamingResponse( ResponseBase ):
 
 app = Flask(__name__)
 app.debug = True
-app.config['MIME_TYPE'] = "audio/mpeg"
 
 @app.route('/')
 def index():
@@ -118,31 +129,47 @@ def index():
 @app.route('/stream/<path:path>')
 def stream( path ):
     
-    mimetype = app.config['MIME_TYPE']
-    
-    # don't start streaming until we've been asked properly for audio data
-    if request.accept_mimetypes[ mimetype ] < 1:
-        print "-> return dummy stream"
-        def stream():
-            yield '0'
-        return Response( stream(), mimetype=mimetype )
-
-    print "content accepted"
-
+    # check we've got the path format we're expecting    
     path_components = path.split("/")
     if len(path_components) < 1:
         #TODO: more detailed error throwing
         abort(400)
 
-    youtube_id = path_components[0]
+    # separate the 'filename' into root & ext
+    filename = path_components[0]
+    root, ext = os.path.splitext( filename )
+    youtube_id = root
     if not re.match( r'[a-zA-Z0-9_-]{11}', youtube_id ):
         #TODO: more detailed error throwing
         abort(400)
 
-    r = StreamingResponse( youtube_id=youtube_id )
+
+    mimetype = MIME_TYPE
+    audio_format = ext[1:]
+    headers = {
+        # 'Content-Disposition' : 'attachment; filename=%s' % filename,
+    }
+
+    fs = 0
+    if audio_format == AUDIO_FORMAT_MP3:
+        d = request.args.get("d")
+        if d:
+            fs = get_filesize( d ) 
+        headers['Content-length'] = fs
+
+    # don't start streaming until we've been asked properly for audio data
+    if request.accept_mimetypes[ mimetype ] < 1:
+        print "-> return dummy stream"
+        def stream():
+            yield '0'
+        return Response( stream(), mimetype=mimetype, headers=headers )
+
+    print "content accepted"
+    r = StreamingResponse( youtube_id=youtube_id, audio_format=audio_format, headers=headers )
     return r
 
 def format_duration( seconds ):
+    seconds = int(seconds)
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)    
@@ -160,6 +187,45 @@ def parse_categories( tag ):
     categories = [ category for category in categories if category in valid_categories ]
     return categories
 
+def get_subtitle( text ):
+    subtitle = ""
+    if (text is not None) and (len(text) > 255):
+        subtitle = text[:255]
+    return subtitle
+
+def get_comments( entry_tag ):
+    comments = ""
+    try:
+        comments_tag = entry_tag.find("gd:comments")
+        comments_link = comments_tag.find("gd:feedLink")
+        comments = comments_link.attrs["href"]
+    except Exception, e:
+        pass
+    return comments
+
+def get_filesize( duration ):
+    if AUDIO_FORMAT == AUDIO_FORMAT_MP3:
+        duration = int(duration)
+        return (duration * (MP3_BITRATE_K*1000)) / 8
+    return 0
+
+def get_media_url( yt_id, duration=0 ):
+    media_url = None
+    fn_media = "%s.%s" % ( yt_id, AUDIO_FORMAT )
+    try:
+        media_url = url_for( "stream", path=fn_media, _external=True, d=duration )
+    except Exception, e:
+        pass
+    return media_url 
+
+def get_feed_url( playlist_id ):
+    feed_url = None
+    try:
+        feed_url = url_for( "feed", path=playlist_id, _external=True )
+    except Exception, e:
+        pass
+    return( feed_url )
+
 def parse_playlist( playlist_id ):
 
     feed_url = "http://gdata.youtube.com/feeds/api/playlists/" + playlist_id
@@ -173,13 +239,10 @@ def parse_playlist( playlist_id ):
     
     channel_author = feed.find("author")
     channel_title = "tubecast: %s" % feed.find("title").string
-    channel_subtitle = feed.find("subtitle")
-    if channel_subtitle is not None:
-        channel_subtitle = channel_subtitle.string
-        if (channel_subtitle is not None) and (len(channel_subtitle) > 255):
-            channel_subtitle = channel_subtitle[:255]
+    channel_subtitle = feed.find("subtitle").string
     if channel_subtitle is None:
         channel_subtitle = ""
+    channel_description = channel_subtitle
 
     self_url = None
     try:
@@ -190,7 +253,7 @@ def parse_playlist( playlist_id ):
     channel_out["title"] = channel_title
     channel_out["subtitle"] = channel_subtitle
     channel_out["link"] = feed.find( "link", rel="alternate" ).attrs["href"]
-    channel_out["description"] = feed.find("subtitle").string
+    channel_out["description"] = channel_description
     channel_out["last_updated"] = format_date( feed.find("updated").string )
     channel_out["author"] = channel_author
     channel_out["author_name"] = channel_author.find("name").text
@@ -207,7 +270,6 @@ def parse_playlist( playlist_id ):
     channel_out["keywords"] = ",".join([ "tubecast" ])
     channel_out["categories"] = parse_categories( feed )
 
-
     for entry_tag in soup.find_all( "entry" ):
 
         this_item = {}
@@ -218,7 +280,9 @@ def parse_playlist( playlist_id ):
         player_url = media_player.attrs[ "url" ]
         item_author = entry_tag.find("author")
         item_duration = media_group.find( "media:content" ).attrs["duration"]
-        
+        item_description = entry_tag.find( "yt:description" ).text
+        item_subtitle = get_subtitle( item_description )
+
         # get youtube item id from player url
         parsed = urlparse.urlparse( player_url )
         qd = urlparse.parse_qs( parsed.query )
@@ -226,34 +290,38 @@ def parse_playlist( playlist_id ):
         
         # construct item dict
         this_item["title"] = entry_tag.find("title").string
-        this_item["subtitle"] = ""
+        this_item["subtitle"] = item_subtitle
         this_item["pub_date"] = format_date( entry_tag.find("published").string )
         this_item["guid"] = entry_tag.find("id").string
-        this_item["content"] = entry_tag.find("content").string
+        this_item["content"] = item_description
+        this_item["comments"] = get_comments( entry_tag )
         
         this_item["link"] = entry_tag.find( "link", rel="alternate" ).attrs["href"]
-        media_url = None
-        try:
-            media_url = url_for( "stream", path=yt_id, _external=True )
-        except Exception, e:
-            pass
-        this_item["media_url"] = media_url
+        this_item["media_url"] = get_media_url( yt_id, item_duration )
         this_item["length"] = 0
 
         this_item["author"] = item_author
         this_item["author_name"] = item_author.find("name").text
         
-        this_item["keywords"] = this_item["categories"] = parse_categories( entry_tag )
+        keywords = ""
+        try:
+            keywords = media_group.find("keywords").text
+        except Exception, e:
+            pass
+        this_item["keywords"] = keywords
+        this_item["categories"] = parse_categories( entry_tag )
 
         this_item["duration"] = item_duration
-        this_item["description"] = media_group.find( "media:description" ).string
+        this_item["description"] = item_description
 
         this_item["mime_type"] = app.config["MIME_TYPE"]
-        this_item["it_duration"] = format_duration( int(item_duration) )
+        this_item["it_duration"] = format_duration( item_duration )
+        this_item["filesize"] = get_filesize( item_duration )
         
         items_out.append( this_item )
 
     return channel_out, items_out
+
 
 @app.route('/feed/<path:path>')
 def feed( path ):
@@ -266,13 +334,15 @@ def feed( path ):
         #TODO: better validation
         abort(400)
 
+    #rendered = generate_feed( playlist_id )
+
     try:
         channel, items = parse_playlist( playlist_id )
     except Exception:
         #TODO: better error handling
         abort(500)
     
-    rendered = render_template( "feed_itunes.xml",
+    rendered = render_template( "feed.xml",
                             channel=channel,
                             items=items )
     
